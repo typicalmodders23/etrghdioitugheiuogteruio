ed71a0339ea73a0206efb8c70e7a00875018035a
typicalmodders23 <70477972+typicalmodders23@users.noreply.github.com>
2023-10-18 19:35:03 +0100

    ergageregraegar

--- app/main/urls.py
+++ app/main/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from cat.views import catt
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',catt)
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
